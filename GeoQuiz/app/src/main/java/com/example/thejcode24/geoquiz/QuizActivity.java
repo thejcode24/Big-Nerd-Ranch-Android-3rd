e4e9da2d8aaf6c86ee31e004dccda2690e5b95a6
James <thejcode24@gmail.com>
2018-09-14 12:08:28 -0700

    added question list and button to move to next question

--- GeoQuiz/app/src/main/java/com/example/thejcode24/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/example/thejcode24/geoquiz/QuizActivity.java
@@ -2,9 +2,9 @@ package com.example.thejcode24.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
@@ -12,11 +12,37 @@ public class QuizActivity extends AppCompatActivity {
     //m prefix on the two member (instance) variable names
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+    private int mCurrentIndex = 0;
+
+
+    //create array of Question class and insert string of questions
+    //by calling Question constructors with question resource id
+    private Question[] mQuestionBank = new Question[] {
+            new Question(R.string.question_australia, true),
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true),
+            new Question(R.string.question_korea, true),
+            new Question(R.string.question_vatican, true),
+            new Question(R.string.question_turkey, false),
+            new Question(R.string.question_paris, true),
+
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        // get reference for TextView and set its text to the question at current index
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
 
         mTrueButton = (Button) findViewById(R.id.true_button);
         // listener for button click action
@@ -42,6 +68,17 @@ public class QuizActivity extends AppCompatActivity {
                         Toast.LENGTH_SHORT).show();
             }
         });
+
+        mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex+1) % mQuestionBank.length;
+                int question = mQuestionBank[mCurrentIndex].getTextResId();
+                mQuestionTextView.setText(question);
+            }
+        });
+
 
     }
 }
